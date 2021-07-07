55e22e33125e7f120cc7d484df7dbb9e5522e8bc
John Medlong <synthmusic@gmail.com>
2021-07-06 21:52:06 -0700

    personal modifications for productivity
    - set defaults to personal
    - auto-update the PUG box
    - add copy button
    - remove tracking code
    - updated most dependencies to latest
    - removed vercel references
    - clear html and focus
    
    diffs are off because of formatter - must use character diffs

--- src/index.js
+++ src/index.js
@@ -4,23 +4,26 @@ import CodeMirror from "codemirror/lib/codemirror.js";
 import { convert as xhtml2pug } from "xhtml2pug";
 
 import {
-  ready,
-  getFromStorage,
-  saveToStorage,
-  setOpacityForInput,
-  setParamsFromStorage,
-  collectOptions,
+    ready,
+    getFromStorage,
+    saveToStorage,
+    setOpacityForInput,
+    setParamsFromStorage,
+    collectOptions,
 } from "./helpers";
 import { EXAMPLE_HTML } from "./example-html";
 
 const mapOfElements = {
-  nSpaces: document.querySelector("#spaceValue"),
-  tabs: document.querySelector("#tabs"),
-  bodyLess: document.querySelector("#bodyLess"),
-  attrComma: document.querySelector("#attrComma"),
-  encode: document.querySelector("#encode"),
-  doubleQuotes: document.querySelector("#doubleQuotes"),
-  inlineCSS: document.querySelector("#inlineCSS"),
+    nSpaces: document.querySelector("#spaceValue"),
+    tabs: document.querySelector("#tabs"),
+    bodyLess: document.querySelector("#bodyLess"),
+    attrComma: document.querySelector("#attrComma"),
+    encode: document.querySelector("#encode"),
+    doubleQuotes: document.querySelector("#doubleQuotes"),
+    inlineCSS: document.querySelector("#inlineCSS"),
+    copyInput: document.querySelector("#copyInput"),
+    copyPug: document.querySelector("#copyPug"),
+    clearHtml: document.querySelector("#clearHtml"),
 };
 
 /**
@@ -35,59 +38,79 @@ const mapOfElements = {
  */
 
 ready(function () {
-  const restoredParams = getFromStorage();
-  if (restoredParams) {
-    setParamsFromStorage(restoredParams, mapOfElements);
-  }
+    const restoredParams = getFromStorage();
+    if (restoredParams) {
+        setParamsFromStorage(restoredParams, mapOfElements);
+    }
 
-  /**
-   * Creating html editor
-   * @type {CodeMirror.Editor}
-   */
-  const htmlEditor = CodeMirror(document.querySelector("#html"), {
-    value: (restoredParams && restoredParams.html) || EXAMPLE_HTML,
-    mode: "htmlmixed",
-    lineNumbers: true,
-  });
+    /**
+     * Creating html editor
+     * @type {CodeMirror.Editor}
+     */
+    const htmlEditor = CodeMirror(document.querySelector("#html"), {
+        value: (restoredParams && restoredParams.html) || EXAMPLE_HTML,
+        mode: "htmlmixed",
+        lineNumbers: true,
+    });
+    htmlEditor.on("change", collectAndConvert);
 
-  /**
-   * Creating pug editor
-   * @type {CodeMirror.Editor}
-   */
-  const pugEditor = CodeMirror(document.querySelector("#pug"), {
-    mode: "pug",
-    lineNumbers: true,
-  });
+    /**
+     * Creating pug editor
+     * @type {CodeMirror.Editor}
+     */
+    const pugEditor = CodeMirror(document.querySelector("#pug"), {
+        mode: "pug",
+        lineNumbers: true,
+    });
 
-  /**
-   * Post text to api for convert html to pug
-   * @param {string} html
-   * @param {Options} options
-   */
-  function convert(html, { tabs, nSpaces, ...options }) {
-    const data = { html, options };
-    const result = xhtml2pug(html, {
-      ...options,
-      symbol: tabs ? '\t' : ' '.repeat(nSpaces)
-    })
-    pugEditor.setValue(result);
-    saveToStorage(data);
-  }
+    /**
+     * Post text to api for convert html to pug
+     * @param {string} html
+     * @param {Options} options
+     */
+    function convert(html, { tabs, nSpaces, ...options }) {
+        const data = { html, options };
+        const result = xhtml2pug(html, {
+            ...options,
+            symbol: tabs ? "\t" : " ".repeat(nSpaces),
+        });
+        pugEditor.setValue(result);
+        saveToStorage(data);
+    }
 
-  /**
-   * Если выбраны табы, то spaceValueField скрыто
-   */
-  document
-    .getElementById("tabs-or-space")
-    .addEventListener("click", setOpacityForInput);
+    /**
+     * Если выбраны табы, то spaceValueField скрыто
+     */
+    document.getElementById("tabs-or-space").addEventListener("click", setOpacityForInput);
 
-  /**
-   * По кнопке передаю все в функцию
-   */
-  document.querySelector("#convert").onclick = function () {
-    const html = htmlEditor.getValue();
-    const options = collectOptions(mapOfElements);
+    /**
+     * По кнопке передаю все в функцию
+     */
+    document.querySelector("#convert").onclick = collectAndConvert;
 
-    convert(html, options);
-  };
+    function collectAndConvert() {
+        let html = htmlEditor.getValue();
+        const options = collectOptions(mapOfElements);
+
+        convert(html || " ", options);
+    }
+
+    copyPug.addEventListener("click", clickCopyPug);
+
+    function clickCopyPug(event) {
+        event.preventDefault();
+
+        copyInput.value = pugEditor.getValue();
+        copyInput.select();
+        document.execCommand("copy");
+    }
+
+    clearHtml.addEventListener("click", clickClearHtml);
+
+    function clickClearHtml(event) {
+        event.preventDefault();
+
+        htmlEditor.setValue("");
+        htmlEditor.focus();
+    }
 });
